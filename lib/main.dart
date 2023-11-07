c24f6c1789282d0e3271835e5fa03c9bd5f5da04
alga89953 <alga89953@gmail.com>
2023-11-07 10:53:57 -0600

    Subiendo nuevo repositorio

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:spotify_sdk/spotify_sdk.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spotify Login',
+      home: SpotifyLoginScreen(),
+    );
+  }
+}
+
+class SpotifyLoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Spotify Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            _loginToSpotify();
+          },
+          child: Text('Login with Spotify'),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _loginToSpotify() async {
+    String clientId = '6c83e4fddc0e4e46bb66d6defcae149e';
+    String redirectUrl = 'http://localhost:3000/spotify_login';
+
+    try {
+      await SpotifySdk.connectToSpotifyRemote(
+        clientId: clientId,
+        redirectUrl: redirectUrl,
+        // Resto de los parámetros opcionales
+      );
+      // Acciones después de iniciar sesión correctamente
+    } catch (e) {
+      // Manejo de errores
+      print('Error al iniciar sesión en Spotify: $e');
+    }
+  }
+}
